19c50316149f69140d9cee86eacb94bc473f5a0b
zhangxv <raingxm@163.com>
2014-06-04 12:56:25 +0800

    复制构造函数的用法

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+class A
+{
+private:
+	int value;
+
+public:
+	A(int n){ value = n;}
+	A(const A& other){value = other.value;}
+
+	void Print(){ std::cout<< value << std::endl; }
+
+};
+
+int main(int argc,char *argv[])
+{
+	A a = 10;
+	A b = a;
+	b.Print();
+	return 0;
+}
